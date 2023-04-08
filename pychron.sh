132916e3986b9beeeca7ca4b9353724c3b17d84a
Jake Ross <jirhiker@gmail.com>
2023-04-08 09:27:17 -0600

    Update pychron.sh

--- pychron.sh
+++ pychron.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 docker container prune -f
 
-# if using mac os
-#========================================================================================
-xhost +127.0.0.1
-docker run --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY=host.docker.internal:0" ghcr.io/nmgrl/pychron:latest
-#========================================================================================
-
-# if using linux comment out the above 2 lines
-# and uncomment the the following 2 lines starting at xhost...
-#========================================================================================
-#xhost +local:root
-#docker run -volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY" --env="QT_X11_NO_MITSHM=1"  pychron3
-#========================================================================================
+if [hostnamectl]
+then
+    # if linux
+    #========================================================================================
+    xhost +local:root
+    docker run -volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" ghcr.io/nmgrl/pychron:latest
+else
+    #========================================================================================
+    # if using mac os
+    #========================================================================================
+    xhost +127.0.0.1
+    docker login ghcr.io
+    docker run --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY=host.docker.internal:0" ghcr.io/nmgrl/pychron:latest
+    #========================================================================================
+fi
