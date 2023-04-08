071288c90c851421d1c8bae37dbd35e275b600fb
Jake Ross <jirhiker@gmail.com>
2023-04-08 10:08:54 -0600

    Update pychron.sh

--- pychron.sh
+++ pychron.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 docker container prune -f
 
-if [ hostnamectl ]
+if [ "$(uname)" == "Darwin" ]
 then
-    # if linux
-    #========================================================================================
-    xhost +local:root
-    docker run --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" ghcr.io/nmgrl/pychron:latest
-else
+    echo "using mac os"
     #========================================================================================
     # if using mac os
     #========================================================================================
@@ -15,4 +11,11 @@ else
     docker login ghcr.io
     docker run --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY=host.docker.internal:0" ghcr.io/nmgrl/pychron:latest
     #========================================================================================
+else
+    
+    echo "using linux"
+    # if linux
+    #========================================================================================
+    xhost +local:root
+    docker run --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" ghcr.io/nmgrl/pychron:latest
 fi
